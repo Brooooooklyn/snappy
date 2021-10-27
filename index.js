00cdd39a9576567620216ad01c8d063bac32ac77
LongYinan <lynweklm@gmail.com>
2021-10-27 23:57:32 +0800

    fix: avoid copy input buffer

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const {
 } = loadBinding(__dirname, 'snappy', '@napi-rs/snappy')
 
 module.exports.compress = function compress(input) {
-  return _compress(Buffer.from(input))
+  return _compress(Buffer.isBuffer(input) ? input : Buffer.from(input))
 }
 
 module.exports.compressSync = function compressSync(input) {
-  return _compressSync(Buffer.from(input))
+  return _compressSync(Buffer.isBuffer(input) ? input : Buffer.from(input))
 }
 
 module.exports.compress = function compress(input) {
-  return _compress(Buffer.from(input))
+  return _compress(Buffer.isBuffer(input) ? input : Buffer.from(input))
 }
 
 module.exports.uncompress = function uncompress(input, opt = { asBuffer: true }) {
